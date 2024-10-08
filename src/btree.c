7909907ba43c5ee82ee6f968bc002dfa7ba6915a
jawdaayyn <akk.jawdanpro@gmail.com>
2024-10-08 17:09:35 -0400

    added btree insertfunction

--- src/btree.c
+++ src/btree.c
@@ -1,5 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct node {
-    int data; 
+    int data;
     struct node *left;
     struct node *right;
 } Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void insert(Node** root, int data) {
+    Node* newNode = createNode(data);  
+    if (*root == NULL) { 
+        *root = newNode;
+        return;
+    }
+
+    Node* current = *root;
+    Node* parent = NULL;
+
+    while (current != NULL) {
+        parent = current;
+
+        if (data < current->data) { 
+            current = current->left;
+            if (current == NULL) {
+                parent->left = newNode;  
+                break;
+            }
+        } else { 
+            current = current->right;
+            if (current == NULL) {
+                parent->right = newNode;  
+                break;
+            }
+        }
+    }
+
+    return;
+}
+
+int main() {
+    Node* root = NULL;
+
+    insert(&root, 20);
+    insert(&root, 10);
+    insert(&root, 30);
+    insert(&root, 5);
+    insert(&root, 15);
+
+    printf("Node's data is %d", root->left->data);
+    return 0;
+}
